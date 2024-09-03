b8a2411fda4db9912fa585ab25a7723037825db7
ysabelli-saraiva <ysabellisaraiva1b3@gmail.com>
2024-09-03 10:32:10 -0300

    Adição de Botões

--- Calculadora.js
+++ Calculadora.js
@@ -1,5 +1,19 @@
 class Calculadora {
     // Criar os Metódos
- 
+ somar(n1,n2){
+   return n1+n2;
+ }
+
+ subtrair(n1,n2){
+    return n1-n2;
+  }
+
+ multiplicar(n1,n2){
+    return n1*n2;
+  }
+
+ dividir(n1,n2){
+    return n1/n2;
+  }
 } 
 
